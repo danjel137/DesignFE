27903f2998ae76f926f3f841047fad3b30dd5c1c
DanjelKyle <107757601+DanjelKyle@users.noreply.github.com>
2023-11-11 10:58:15 +0100

    Finished photo gallery

--- color_markers/src/App.js
+++ color_markers/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import ColoredMarkers from "./ColoredMarkers/ColoredMarkers";
 import RothkoPainting from "./RothkoPainting/RothkoPainting";
+import PhotoGallery from "./PhotoGallery/PhotoGallery";
 
 function App() {
   return (
     <div >
-        <RothkoPainting/>
+        <PhotoGallery/>
     </div>
   );
 }
